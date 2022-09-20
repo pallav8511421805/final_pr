0400887e34211ef62778d388b949852739bedbb8
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-20 18:21:56 +0530

    change

--- src/alert/Alert.js
+++ src/alert/Alert.js
@@ -1,27 +1,30 @@
-import React, { useContext, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useSnackbar } from "notistack";
-import { resetalertaction } from "../redux/actions/alert.action";
+// import React, { useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { useSnackbar } from "notistack";
+// import { resetalertaction } from "../redux/actions/alert.action";
 
-function Alert(props) {
-  const { enqueueSnackbar } = useSnackbar();
-  let alertdata = useSelector((state) => state.alert);
-  let dispatch = useDispatch();
-  useEffect(() => {
-    if (alertdata.text !== "") {
-      enqueueSnackbar(alertdata.text, {
-        variant: alertdata.color,
-        anchorOrigin: {
-          vertical: "top",
-          horizontal: "right",
-        },
-      });
-      setTimeout(() => {
-        dispatch(resetalertaction);
-      }, 2000);
-    }
-  }, [alertdata.text]);
-  return <div></div>;
-}
+// function Alert(props) {
+//   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+//   let alertdata = useSelector((state) => state.alert);
+//   let dispatch = useDispatch();
+//   console.log(alertdata.text);
 
-export default Alert;
+//   useEffect(() => {
+//     if (alertdata.text !== "") {
+//       enqueueSnackbar(alertdata.text, {
+//         variant: alertdata.color,
+//         anchorOrigin: {
+//           vertical: "top",
+//           horizontal: "right",
+//         },
+//       });
+//       setTimeout(() => {
+//         dispatch(resetalertaction);
+//       }, 2000);
+//     }
+
+//   }, [alertdata.text]);
+//   return <div></div>;
+// }
+
+// export default Alert;
