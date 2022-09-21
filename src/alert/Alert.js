b0d6a76441bd4e2da9dfa3fe25cd42218259cce3
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-21 11:49:15 +0530

    change

--- src/alert/Alert.js
+++ src/alert/Alert.js
@@ -12,7 +12,7 @@ function Alert(props) {
       enqueueSnackbar(Alert.text, {
         variant: Alert.color,
         anchorOrigin: {
-          vertical: "top",
+          vertical: "bottom",
           horizontal: "right",
         },
       });
