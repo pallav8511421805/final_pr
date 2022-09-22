964b9280019a1d3a570d20c53acabc6e01f794d6
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-22 12:27:59 +0530

    change

---
+++ src/components/Displaycategory.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+function Displaycategory(props) {
+  let displaydata = useSelector((state) => state.category);
+  return (
+    <>
+      <section className="product_section layout_padding">
+        <div className="container">
+          <div className="heading_container heading_center">
+            <h2>
+              <span>category</span>
+            </h2>
+          </div>
+
+          <div className="row">{console.log(displaydata)}</div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Displaycategory;
