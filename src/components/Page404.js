f4f9ecc625287538394d3101edc7b56d49af29e8
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-26 10:38:40 +0530

    change

--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Page404(props) {
-  return <div>404 page</div>;
+  return (
+    <section style={{ padding: "100px" }}>
+      <div className="container">
+        <div className="d-flex justify-content-center">
+          <img src="images/page404.png" className="imgdata" />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Page404;
