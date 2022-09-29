a8a152f50476f1b910bd22b095eac4f17080dc72
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-29 07:04:07 +0530

    change

--- src/Admin/components/Displayproducts.js
+++ src/Admin/components/Displayproducts.js
@@ -20,57 +20,29 @@ function Displayproducts(props) {
     <>
       <div className="row">
         {data.map((d, i) => {
-          if (i === 0) {
-            return (
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="box">
-                  <div className="option_container">
-                    <div className="options">
-                      <a className="option1" onClick={() => handleview(d.id)}>
-                        View
-                      </a>
-                    </div>
-                  </div>
-                  <div className="img-box">
-                    <img src={d.pname} alt />
-                  </div>
-                  <div className="detail-box">
-                    <h5>{d.name}</h5>
-                    <h6>${d.price}</h6>
+          return (
+            <div className="col-sm-6 col-md-4 col-lg-4">
+              <div className="box">
+                <div className="option_container">
+                  <div className="options">
+                    <a className="option1" onClick={() => handleview(d.id)}>
+                      View
+                    </a>
                   </div>
                 </div>
-                <div className="text-center my-2">
-                  <a className="add_to_cart">
-                    Add to cart{" "}
-                    <i class="fa fa-cart-plus" aria-hidden="true"></i>
-                  </a>
+                <div className="img-box">
+                  <img src={d.pname} alt />
+                </div>
+                <div className="detail-box">
+                  <h5>{d.name}</h5>
+                  <h6>${d.price}</h6>
                 </div>
               </div>
-            );
-          } else {
-            return (
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="box">
-                  <div className="option_container">
-                    <div className="options"></div>
-                  </div>
-                  <div className="img-box">
-                    <img src={d.pname} alt />
-                  </div>
-                  <div className="detail-box">
-                    <h5>{d.name}</h5>
-                    <h6>${d.price}</h6>
-                  </div>
-                </div>
-                <div className="text-center my-2">
-                  <a className="add_to_cart">
-                    Add to cart{" "}
-                    <i class="fa fa-cart-plus" aria-hidden="true"></i>
-                  </a>
-                </div>
+              <div className="text-center my-2">
+                <a className="add_to_cart">+ Add to cart</a>
               </div>
-            );
-          }
+            </div>
+          );
         })}
       </div>
     </>
