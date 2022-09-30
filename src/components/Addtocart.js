b72372a8755d553e339df342c9b745b5b6827245
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-30 10:56:53 +0530

    change

--- src/components/Addtocart.js
+++ src/components/Addtocart.js
@@ -1,15 +1,25 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getproduct_data } from "../redux/actions/product.actions";
 
 function Addtocart(props) {
   const cart = useSelector((state) => state.cartroot);
   const cartdata = cart.cartdata;
-  console.log(cartdata);
-  return (
-    <div>
-      <div>add to cart</div>
-    </div>
-  );
+  const dispatch = useDispatch();
+  const product = useSelector((state) => state.productroot);
+  const data = product.productdata;
+  useEffect(() => {
+    dispatch(getproduct_data());
+  }, []);
+  let CartData = [];
+  data.map((p) => {
+    cartdata.map((c) => {
+      if (p.id === c.id) {
+        CartData.push(p);
+      }
+    });
+  });
+  return <div></div>;
 }
 
 export default Addtocart;
