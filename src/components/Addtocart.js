0fdefb05508560c0e7892b79ffe2af722ec675de
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-30 12:53:12 +0530

    change

--- src/components/Addtocart.js
+++ src/components/Addtocart.js
@@ -4,22 +4,20 @@ import { getproduct_data } from "../redux/actions/product.actions";
 
 function Addtocart(props) {
   const cart = useSelector((state) => state.cartroot);
-  const cartdata = cart.cartdata;
   const dispatch = useDispatch();
   const product = useSelector((state) => state.productroot);
-  const data = product.productdata;
-  useEffect(() => {
-    dispatch(getproduct_data());
-  }, []);
+
   let CartData = [];
-  data.map((p) => {
-    cartdata.map((c) => {
+  product.productdata.map((p) => {
+    cart.cartdata.map((c) => {
       if (p.id === c.id) {
         CartData.push(p);
       }
     });
   });
-  console.log(CartData);
+  useEffect(() => {
+    dispatch(getproduct_data());
+  }, []);
   return (
     <>
       <section className="product_section layout_padding">
@@ -40,19 +38,19 @@ function Addtocart(props) {
                 </div>
               </div>
             </div>
-            {CartData.map((d, i) => {
+            {CartData.map((c) => {
               return (
                 <div className="col-12">
                   <div className="box text-center">
                     <div className="row justify-context-between">
                       <div className="col-4">
-                        <img src={d.pname} width="50px" />
+                        <img src={c.pname} width="50px" />
                       </div>
                       <div className="col-4">
-                        <div>{d.qty}</div>
+                        <div>{c.qty}</div>
                       </div>
                       <div className="col-4">
-                        <div>${d.price}</div>
+                        <div>${c.price}</div>
                       </div>
                     </div>
                   </div>
