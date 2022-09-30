857a47521337d664197c05a14db1811a28c63225
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-09-30 12:16:06 +0530

    change

--- src/components/Addtocart.js
+++ src/components/Addtocart.js
@@ -19,7 +19,51 @@ function Addtocart(props) {
       }
     });
   });
-  return <div></div>;
+  console.log(CartData);
+  return (
+    <>
+      <section className="product_section layout_padding">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="text-center">
+                <div className="row justify-context-between">
+                  <div className="col-4">
+                    <div className="add_to_data">Product</div>
+                  </div>
+                  <div className="col-4">
+                    <div className="add_to_data">Quantity</div>
+                  </div>
+                  <div className="col-4">
+                    <div className="add_to_data">Total</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {CartData.map((d, i) => {
+              return (
+                <div className="col-12">
+                  <div className="box text-center">
+                    <div className="row justify-context-between">
+                      <div className="col-4">
+                        <img src={d.pname} width="50px" />
+                      </div>
+                      <div className="col-4">
+                        <div>{d.qty}</div>
+                      </div>
+                      <div className="col-4">
+                        <div>${d.price}</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Addtocart;
