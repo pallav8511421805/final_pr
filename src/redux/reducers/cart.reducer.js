36626f56a5d58b7fc684f7a77d3f458a5a3e4ee4
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-03 10:39:53 +0530

    change

--- src/redux/reducers/cart.reducer.js
+++ src/redux/reducers/cart.reducer.js
@@ -6,14 +6,21 @@ const intval = {
 export const cartreducer = (state = intval, action) => {
   switch (action.type) {
     case Actiontypes.Add_cart:
+      const itemInCart = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      if (itemInCart) {
+        itemInCart.qty++;
+      } else {
+        state.cart.push({ ...action.payload, qty: 1 });
+      }
       return {
         ...state,
-        cartdata: state.cartdata.concat(action.payload),
+        // cartdata: state.cartdata.concat(action.payload),
       };
       break;
 
     default:
-      
       return state;
       break;
   }
