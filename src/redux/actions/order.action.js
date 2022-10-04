ab22ff103f51684c39d2f956d0c13feb30000a6f
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-04 16:19:13 +0530

    change

--- src/redux/actions/order.action.js
+++ src/redux/actions/order.action.js
@@ -1,5 +1,19 @@
 import * as Actiontypes from "../actiontypes";
-export const Getorderaction = () => (dispatch) => {};
-export const Addorderaction = (data) => (dispatch) => {
-  console.log(data);
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import { db } from "../../Firebase";
+export const Getorderaction = () => async (dispatch) => {
+  let data = [];
+  const querySnapshot = await getDocs(collection(db, "Order"));
+  querySnapshot.forEach((doc) => {
+    data.push({ ...doc.data(), id: doc.id });
+  });
+  dispatch({ type: Actiontypes.Getorder, payload: data });
+};
+
+export const Addorderaction = (data) => async (dispatch) => {
+  const docRef = await addDoc(collection(db, "Order"), {
+    ...data,
+  });
+  console.log("Document written with ID: ", docRef.id);
+  dispatch({ type: Actiontypes.Addorder, payload: { ...data, id: docRef.id } });
 };
