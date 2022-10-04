9bb13c24e8ce1757f78b01276db4d802096f4f38
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-04 18:32:53 +0530

    change

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -5,6 +5,22 @@ import { Getorderaction } from "../../redux/actions/order.action";
 function Orderdata(props) {
   const orderdata = useSelector((state) => state.orderroot);
   const dispaly_order = orderdata.orderdata;
+  console.log(dispaly_order);
+  const dispaly_table = [];
+  const data_table = [];
+  dispaly_order.map((d) => {
+    data_table.push(d);
+  });
+  data_table.map((d, i) => {
+    const orderdata = {
+      ...d[i],
+      d_address: d.address,
+      d_email: d.email,
+      d_fname: d.fname,
+      d_phone: d.phone,
+    };
+    dispaly_table.push(orderdata);
+  });
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(Getorderaction());
@@ -13,7 +29,7 @@ function Orderdata(props) {
   return (
     <div style={{ height: 400, width: "80%", margin: "auto" }}>
       <DataGrid
-        rows={dispaly_order}
+        rows={dispaly_table}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
