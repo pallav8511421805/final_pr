ce98d1b3e3b3550cfb3539097c7bef4905a1ed7b
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-04 19:09:09 +0530

    change

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -5,48 +5,15 @@ import { Getorderaction } from "../../redux/actions/order.action";
 function Orderdata(props) {
   const orderdata = useSelector((state) => state.orderroot);
   const dispaly_order = orderdata.orderdata;
-  console.log(dispaly_order);
-  const dispaly_table = [];
-  const data_table = [];
-  dispaly_order.map((d) => {
-    data_table.push(d);
-  });
-  data_table.map((d, i) => {
-    const orderdata = {
-      ...d[i],
-      d_address: d.address,
-      d_email: d.email,
-      d_fname: d.fname,
-      d_phone: d.phone,
-    };
-    console.log(orderdata);
-    dispaly_table.push(orderdata);
-  });
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(Getorderaction());
   }, []);
-  const columns = [
-    { field: "d_fname", headerName: "User Name", width: 130 },
-    { field: "d_email", headerName: "Email", width: 130 },
-    { field: "d_phone", headerName: "Mobile", width: 130 },
-    { field: "d_address", headerName: "Address", width: 130 },
-    { field: "qty", headerName: "Qty", width: 130 },
-    {
-      field: "pname",
-      headerName: "product image",
-      width: 130,
-      renderCell: (params) => (
-        <>
-          <img src={params.row.pname} width={50} height={50} />
-        </>
-      ),
-    },
-  ];
+  const columns = [{ field: "fname", headerName: "User Name", width: 130 }];
   return (
     <div style={{ height: 400, width: "80%", margin: "auto" }}>
       <DataGrid
-        rows={dispaly_table}
+        rows={dispaly_order}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
