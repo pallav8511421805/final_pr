48de42bd38b03d7615ca9c711ae5501481b4faad
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-04 10:22:41 +0530

    change

--- src/components/Placeorder.js
+++ src/components/Placeorder.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function Placeorder(props) {
-  return <div></div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-6 ">
+            <div className="w-100">
+              <form></form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Placeorder;
