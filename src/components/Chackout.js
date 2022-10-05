99d4d22950fbf51d55e08bb5c0146fad2a53ad06
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-05 10:06:07 +0530

    change

--- src/components/Chackout.js
+++ src/components/Chackout.js
@@ -5,12 +5,19 @@ function Chackout(props) {
     <section>
       <div className="container">
         <div className="row justify-content-center">
-          <div className="col-12 col-md-6 cheack_box">
-            <div className="w-100 my-3">
+          <div className="col-12 col-md-6 cheack_box my-5">
+            <div className="w-100">
               <h1>
                 <span>Thank you ,</span> Your order is placed, vist again
               </h1>
-              <p></p>
+              <p className="my-2">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+              </p>
             </div>
           </div>
         </div>
