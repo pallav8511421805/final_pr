5b1d8b2f05499588eb047850daa4accc65d4a08e
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-06 17:13:27 +0530

    change

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -8,77 +8,112 @@ function Orderdata(props) {
     dispatch(Getorderaction());
   }, []);
 
+  const cartorder = [];
+
   const order = useSelector((state) => state.orderroot);
-  // console.log(order.isload, order.error);
   order.orderdata.map((d) => {
-    console.log(d.fname, d.address, d.phone, d.email);
-    d.cartorder.map((c) => {
-      console.log(c.name, c.qty, c.price, c.pname);
-    });
+    cartorder.push(d.cartorder);
+  });
+
+  cartorder.map((d) => {
+    console.log(d);
   });
   return (
     <>
-      {order.isload ? (
-        <div style={{ fontSize: 24, color: "#1976d2", textAlign: "center" }}>
-          Loading...
-        </div>
-      ) : order.error != "" ? (
-        <div style={{ fontSize: 24 }}>{order.error}</div>
-      ) : (
-        <>
-          <section>
-            <div className="container">
-              <div className="row justify-content-center">
-                <div className="col-12 col-md-5 col-lg-10 col-xl-10">
-                  <div className="w-100">
-                    <table className="addtotable">
-                      <tr className="row justify-content-between trcolor">
-                        <th>User name</th>
-                        <th>Email</th>
-                        <th>Phone</th>
-                        <th>Address</th>
-                        <th>Product</th>
-                        <th>Product name</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                      </tr>
-                      <tbody>
-                        <tr className="row justify-content-between trcolor1">
-                          {order.orderdata.map((d) => {
-                            return (
-                              <>
-                                <td>{d.fname}</td>
-                                <td>{d.email}</td>
-                                <td>{d.phone}</td>
-                                <td>{d.address}</td>
-                              </>
-                            );
-                          })}
-                          {order.orderdata.map((d) => {
-                            d.cartorder.map((c) => {
-                              return (
-                                <>
-                                  <td>
-                                    <img src={c.pname} width={50} />
-                                  </td>
-                                  <td>{c.name}</td>
-                                  <td>{c.qty}</td>
-                                  <td>{c.price}</td>
-                                </>
-                              );
-                            });
-                          })}
-                        </tr>
-                        <tr className="row justify-content-between trcolor2"></tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-5 col-lg-10 col-xl-10">
+              <div className="w-100">
+                {/* <table className="addtotable">
+                  <tr className="row justify-content-between trcolor">
+                    <th>User name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Product</th>
+                    <th>Product name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                  <tbody>
+                    <tr className="row justify-content-between trcolor1">
+                      {order.orderdata.map((d) => {
+                        return (
+                          <>
+                            <td>{d.fname}</td>
+                            <td>{d.email}</td>
+                            <td>{d.phone}</td>
+                            <td>{d.address}</td>
+                          </>
+                        );
+                      })}
+                      {order.orderdata.map((d) => {
+                        d.cartorder.map((c) => {
+                          return (
+                            <>
+                              <td>
+                                <img src={c.pname} width={50} />
+                              </td>
+                              <td>{c.name}</td>
+                              <td>{c.qty}</td>
+                              <td>{c.price}</td>
+                            </>
+                          );
+                        });
+                      })}
+                    </tr>
+                    <tr className="row justify-content-between trcolor2"></tr>
+                  </tbody>
+                </table> */}
+
+                <table
+                  style={{
+                    width: "100%",
+                    color: "black",
+                  }}
+                  border="1"
+                >
+                  <tr className="px-3 text-black border-1 border-primary text-center">
+                    <th>User name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Product</th>
+                    <th>Product name</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                  </tr>
+                  {/* <tr className="px-3">
+                    <td rowSpan={8}>czcx</td>
+                    <td rowSpan={8}>Zxxzxz</td>
+                    <td rowSpan={8}>xhnhcx</td>
+                    <td rowSpan={8}>cxknz</td>
+                  </tr>
+                  <tr className="px-3">
+                    <td>sxznzc</td>
+                    <td>jjc</td>
+                    <td>hhcs</td>
+                    <td>zxhhX</td>
+                  </tr>
+                  <tr className="px-3">
+                    <td>sxznzc</td>
+                    <td>jjc</td>
+                    <td>hhcs</td>
+                    <td>zxhhX</td>
+                  </tr>
+                  <tr className="px-3">
+                    <td>sxznzc</td>
+                    <td>jjc</td>
+                    <td>hhcs</td>
+                    <td>zxhhX</td>
+                  </tr> */}
+                </table>
               </div>
             </div>
-          </section>
-        </>
-      )}
+          </div>
+        </div>
+      </section>
     </>
   );
 }
