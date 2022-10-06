52ada8d1a664f17b76c885d598eca204115d7fed
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-06 20:03:53 +0530

    chaange

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -42,52 +42,70 @@ function Orderdata(props) {
   });
   return (
     <>
-      <section>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-md-5 col-lg-10 col-xl-10">
-              <div className="w-100">
-                <table
-                  style={{
-                    width: "100%",
-                    color: "black",
-                  }}
-                  border="1"
-                >
-                  <tr className="px-3 text-black border-1 border-primary text-center">
-                    <th>User name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                    <th>Product</th>
-                    <th>Product name</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                  </tr>
-                  <tbody>
-                    {Cartorderdata.map((d) => {
-                      return (
-                        <tr className="text-center">
-                          <td>{d.fname}</td>
-                          <td>{d.email}</td>
-                          <td>{d.phone}</td>
-                          <td>{d.address}</td>
-                          <td>
-                            <img src={d.pname} width={50} />
-                          </td>
-                          <td>{d.name}</td>
-                          <td>{d.qty}</td>
-                          <td>{d.price}</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
+      {order.isload ? (
+        <div className="text-center text-primary">Loading....</div>
+      ) : order.error != "" ? (
+        <div className="text-center text-primary">{order.error}</div>
+      ) : (
+        <>
+          <section>
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-5 col-lg-10 col-xl-10">
+                  <div className="w-100">
+                    <table
+                      style={{
+                        width: "100%",
+                        color: "black",
+                        border: "1px solid black",
+                      }}
+                    >
+                      <tr
+                        className="px-3 text-black text-center"
+                        style={{
+                          border: "1px solid black",
+                        }}
+                      >
+                        <th>User name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Product</th>
+                        <th>Product name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                      </tr>
+                      <tbody>
+                        {Cartorderdata.map((d) => {
+                          return (
+                            <tr
+                              className="px-3 text-black text-center"
+                              style={{
+                                border: "1px solid black",
+                              }}
+                            >
+                              <td>{d.fname}</td>
+                              <td>{d.email}</td>
+                              <td>{d.phone}</td>
+                              <td>{d.address}</td>
+                              <td>
+                                <img src={d.pname} width={50} />
+                              </td>
+                              <td>{d.name}</td>
+                              <td>{d.qty}</td>
+                              <td>{d.price}</td>
+                            </tr>
+                          );
+                        })}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
+          </section>
+        </>
+      )}
     </>
   );
 }
