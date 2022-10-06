2b2bd3655e43ab168c147679c0dff6d84955b771
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-06 14:14:35 +0530

    change

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -9,9 +9,13 @@ function Orderdata(props) {
   }, []);
 
   const order = useSelector((state) => state.orderroot);
-
-  console.log(order.orderdata);
-
+  // console.log(order.isload, order.error);
+  order.orderdata.map((d) => {
+    console.log(d.fname, d.address, d.phone, d.email);
+    d.cartorder.map((c) => {
+      console.log(c.name, c.qty, c.price, c.pname);
+    });
+  });
   return (
     <>
       {order.isload ? (
@@ -22,53 +26,57 @@ function Orderdata(props) {
         <div style={{ fontSize: 24 }}>{order.error}</div>
       ) : (
         <>
-          {/* <section>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-12 col-md-5 col-lg-10 col-xl-10">
-              <div className="w-100">
-                <table className="addtotable">
-                  <tr className="row justify-content-between trcolor">
-                    <th>Products</th>
-                    <th>Quantity</th>
-                    <th>Price</th>
-                    <th>User name</th>
-                    <th>Email</th>
-                    <th>Phone</th>
-                    <th>Address</th>
-                  </tr>
-                  <tbody>
-                    <tr className="row justify-content-between trcolor1">
-                      {dispaly_order.cartorder.map((d) => {
-                        return (
-                          <>
-                            <td className="align-self-center">{d.name}</td>
-                            <td className="align-self-center">{d.qty}</td>
-                            <td className="align-self-center">{d.price}</td>
-                          </>
-                        );
-                      })}
-                      <td className="align-self-center">
-                        {dispaly_order.fname}
-                      </td>
-                      <td className="align-self-center">
-                        {dispaly_order.email}
-                      </td>
-                      <td className="align-self-center">
-                        {dispaly_order.phone}
-                      </td>
-                      <td className="align-self-center">
-                        {dispaly_order.address}
-                      </td>
-                    </tr>
-                    <tr className="row justify-content-between trcolor2"></tr>
-                  </tbody>
-                </table>
+          <section>
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-12 col-md-5 col-lg-10 col-xl-10">
+                  <div className="w-100">
+                    <table className="addtotable">
+                      <tr className="row justify-content-between trcolor">
+                        <th>User name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Address</th>
+                        <th>Product</th>
+                        <th>Product name</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                      </tr>
+                      <tbody>
+                        <tr className="row justify-content-between trcolor1">
+                          {order.orderdata.map((d) => {
+                            return (
+                              <>
+                                <td>{d.fname}</td>
+                                <td>{d.email}</td>
+                                <td>{d.phone}</td>
+                                <td>{d.address}</td>
+                              </>
+                            );
+                          })}
+                          {order.orderdata.map((d) => {
+                            d.cartorder.map((c) => {
+                              return (
+                                <>
+                                  <td>
+                                    <img src={c.pname} width={50} />
+                                  </td>
+                                  <td>{c.name}</td>
+                                  <td>{c.qty}</td>
+                                  <td>{c.price}</td>
+                                </>
+                              );
+                            });
+                          })}
+                        </tr>
+                        <tr className="row justify-content-between trcolor2"></tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section> */}
+          </section>
         </>
       )}
     </>
