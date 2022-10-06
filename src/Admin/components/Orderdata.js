68dd2764efd5f5cde1605818fdaa4325a910ee6b
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-06 20:24:07 +0530

    cahnge

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -11,17 +11,17 @@ function Orderdata(props) {
   const order = useSelector((state) => state.orderroot);
 
   let Cartorder = [];
-  let Cartorder1 = [];
   let Cartorderdata = [];
 
+  let obj;
+
   order.orderdata.map((d) => {
-    let obj = {
+    obj = {
       address: d.address,
       phone: d.phone,
       email: d.email,
       fname: d.fname,
     };
-    Cartorder1.push(obj);
   });
 
   order.orderdata.map((d) => {
@@ -33,15 +33,12 @@ function Orderdata(props) {
     });
   });
 
-  Cartorder1.map((d) => {
-    console.log(d);
-    Cartorder.map((x) => {
-      let objdata = {
-        ...d,
-        ...x,
-      };
-      Cartorderdata.push(objdata);
-    });
+  Cartorder.map((d) => {
+    let objdata = {
+      ...obj,
+      ...d,
+    };
+    Cartorderdata.push(objdata);
   });
   return (
     <>
