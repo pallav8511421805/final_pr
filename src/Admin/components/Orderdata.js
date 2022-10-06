35f9cdad9a511f46711325f81504d5f4b7b98d20
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-06 11:40:51 +0530

    change

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -12,11 +12,53 @@ function Orderdata(props) {
 
   return (
     <>
-      <div style={{ height: 400, width: "80%", margin: "auto" }}>
-        <table style={{ width: "80" }}>
-          <th>fhhdf</th>
-        </table>
-      </div>
+      <section>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-12 col-md-5 col-lg-10 col-xl-10">
+              <div className="w-100">
+                <table className="addtotable">
+                  <tr className="row justify-content-between trcolor">
+                    <th>Products</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>User name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                  </tr>
+                  <tbody>
+                    <tr className="row justify-content-between trcolor1">
+                      {dispaly_order.cartorder.map((d) => {
+                        return (
+                          <>
+                            <td className="align-self-center">{d.name}</td>
+                            <td className="align-self-center">{d.qty}</td>
+                            <td className="align-self-center">{d.price}</td>
+                          </>
+                        );
+                      })}
+                      <td className="align-self-center">
+                        {dispaly_order.fname}
+                      </td>
+                      <td className="align-self-center">
+                        {dispaly_order.email}
+                      </td>
+                      <td className="align-self-center">
+                        {dispaly_order.phone}
+                      </td>
+                      <td className="align-self-center">
+                        {dispaly_order.address}
+                      </td>
+                    </tr>
+                    <tr className="row justify-content-between trcolor2"></tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
