e942f436a1f06819b18259b645c5a3fec0f09f4b
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-07 19:26:33 +0530

    change

--- src/Admin/components/Orderdata.js
+++ src/Admin/components/Orderdata.js
@@ -63,15 +63,16 @@ function Orderdata(props) {
                                 {d.cartorder.map((x) => {
                                   return (
                                     <>
-                                    <div className="row">
+                                    
+                                      <div style={{border:'1px solid black'}}>
                                       <img src={x.pname} width={50} className="align-self-center"/>
                                       <div className="align-self-center">
                                       <div className="mx-1">Name : {x.name}</div>
                                       <div className="mx-1">Qty : {x.qty}</div>
                                       <div className="mx-1">Price : {x.price}</div>
                                       </div>
-                                      <br/>
-                                    </div>
+                                      </div>
+                                    
                                     </>
                                   );
                                 })}
