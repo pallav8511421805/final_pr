b0edb62281cff8ee464ae10ba35da0a86f3ab90b
pallav8511421805 <pallavkevadiya8@gmail.com>
2022-10-11 08:39:30 +0530

    change

--- src/apis/auth.api.js
+++ src/apis/auth.api.js
@@ -33,6 +33,7 @@ export const signUpapi = (values) => {
               role:"user",
               emailVerified : user.emailVerified,
             })
+          
           }
         });
       })
